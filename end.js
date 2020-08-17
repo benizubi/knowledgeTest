0de5f56547928bb0bd77cf9990a5047628364007
benizubi <benizubi@outlook.com>
2020-08-17 19:51:18 +0100

    sorting highscore page

--- end.js
+++ end.js
@@ -5,7 +5,7 @@ const mostRecentScore = localStorage.getItem('mostRecentScore');
 // things in local storage are stored as a string
 const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 console.log(highScores);
-// changing the array string to json parse changes a string to an array  to display a list of scores.
+// changing a string to an array object is json parse to display a list of scores.
 const MAX_HIGH_SCORES = 5;
 
 
@@ -22,11 +22,16 @@ saveHighScore = (e) => {
     const score = {
         score: Math.floor(Math.random() * 100),
         name: username.value
+        // collecting thr score and name values to add to the array
     };
     highScores.push(score);
-    highScores.sort((a,b) => b.score - a.score);
-    highScores.splice(5);
+    // pushes this to the array 
 
+    highScores.sort((a,b) => b.score - a.score);
+    //an array function to sort and returning the highest to lowest score usin if stament.
+
+    highScores.splice(5);
+// this only collects the highest score and will slice out anything after the 5th.
     localStorage.setItem("highScores", JSON.stringify(highScores));
     window.location.assign("/");
     
