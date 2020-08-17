612daddc190bb2e235a253215cbb8330eac4fa4e
benizubi <benizubi@outlook.com>
2020-08-17 23:58:09 +0100

    fixed highscore list

--- highscore.js
+++ highscore.js
@@ -1,8 +1,8 @@
-const highScoresList = document.getElementById("highScoreList");
+const highScoresList = document.getElementById("highScoresList");
 const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
 highScoresList.innerHTML = highScores
-highScores
-.map(score =>{
-return `<li class=" high-score">${score.name} - ${score.score}</li>`;
-    
-}).join("");
+  .map(score => {
+    return `<li class="high-score">${score.name} - ${score.score}</li>`;
+  })
+  .join("");
