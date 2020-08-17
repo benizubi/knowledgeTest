cabd9af1b38db0ce1fa11123b3f0d571de491128
benizubi <benizubi@outlook.com>
2020-08-17 19:14:18 +0100

    highscore input sorted

--- end.js
+++ end.js
@@ -2,6 +2,13 @@ const username = document.getElementById('username');
 const saveScoreBtn = document.getElementById('saveScoreBtn');
 const finalScore = document.getElementById('finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
+// things in local storage are stored as a string
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+console.log(highScores);
+// changing the array string to json parse changes a string to an array  to display a list of scores.
+const MAX_HIGH_SCORES = 5;
+
+
 finalScore.innerText = mostRecentScore;
 
 username.addEventListener('keyup', () => {
@@ -9,5 +16,18 @@ username.addEventListener('keyup', () => {
 });
 
 saveHighScore = (e) => {
+    console.log("Clicked the save button!");
     e.preventDefault();
+
+    const score = {
+        score: Math.floor(Math.random() * 100),
+        name: username.value
+    };
+    highScores.push(score);
+    highScores.sort((a,b) => b.score - a.score);
+    highScores.splice(5);
+
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+    window.location.assign("/");
+    
 };
