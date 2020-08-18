f6895c36bf866b716151d63ef93e8205e92fb24f
benizubi <benizubi@outlook.com>
2020-08-18 17:54:23 +0100

    created a spinning loader

--- game.js
+++ game.js
@@ -13,17 +13,37 @@ let questionCounter = 0;
 let availableQuesions = [];
 
 let questions = [];
-fetch("questions.json").then(res => {
-  return res.json();
-})
-.then(loadedQuesyions =>{
-  console.log(loadedQuesyions);
-  questions = loadedQuesyions;
-  startGame();
-})
-.catch(err =>{
-  console.error(err);
-});
+fetch(
+  'https://opentdb.com/api.php?amount=20&category=9&difficulty=medium'
+)
+  .then((res) => {
+      return res.json();
+  })
+  .then((loadedQuestions) => {
+      questions = loadedQuestions.results.map((loadedQuestion) => {
+          const formattedQuestion = {
+              question: loadedQuestion.question,
+          };
+
+          const answerChoices = [...loadedQuestion.incorrect_answers];
+          formattedQuestion.answer = Math.floor(Math.random() * 4) + 1;
+          answerChoices.splice(
+              formattedQuestion.answer - 1,
+              0,
+              loadedQuestion.correct_answer
+          );
+
+          answerChoices.forEach((choice, index) => {
+              formattedQuestion['choice' + (index + 1)] = choice;
+          });
+
+          return formattedQuestion;
+      });
+      startGame();
+  })
+  .catch((err) => {
+      console.error(err);
+  });
 // fetch and always return something to catch e.g error 
 
 ;
